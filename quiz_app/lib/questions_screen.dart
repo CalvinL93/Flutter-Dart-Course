69eaf8c2566501a37090cab8ee197797a4f4438e
CalvinL93 <calvinl93@yahoo.com>
2024-02-05 20:54:57 -0600

    update

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,8 +13,45 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQustionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQustionIndex++;
+    });
+    
+  }
+
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    final currentQustion = questions[currentQustionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              currentQustion.text,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 225, 139, 247),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQustion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: answerQuestion,);
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
