b59b6c40fd6d0b32371712421f33c2c66807a73d
CalvinL93 <calvinl93@yahoo.com>
2024-02-10 22:38:32 -0600

    update

--- expense_app/lib/widgets/expenses.dart
+++ expense_app/lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_app/widgets/chart/chart.dart';
 import 'package:expense_app/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_app/models/expense.dart';
 import 'package:expense_app/widgets/new_expense.dart';
@@ -42,8 +43,41 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
@@ -56,9 +90,9 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The chart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: mainContent,
           ),
         ],
       ),
--- expense_app/lib/widgets/expenses_list/expenses_item.dart
+++ expense_app/lib/widgets/expenses_list/expenses_item.dart
@@ -1,6 +1,5 @@
 import 'package:expense_app/models/expense.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ExpensesItem extends StatelessWidget {
   const ExpensesItem(this.expense, {super.key});
@@ -10,29 +9,37 @@ class ExpensesItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 16,
+        child: Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 16,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Text(
+          expense.title,
+          style: Theme.of(context).textTheme.titleLarge,
         ),
-        child: Column(children: [
-          Text(expense.title),
-          const SizedBox(height: 4,),
-          Row(
-            children: [
-              Text('\$${expense.amount.toStringAsFixed(2)}'),
-              const Spacer(),
-              Row(
-                children: [
-                  Icon(categoryIcons[expense.category]),
-                  const SizedBox(width: 8,),
-                  Text(expense.formattedDate),
-                ],
-              ),
-            ],
-          ),
-        ]),
-      )
-    );
+        const SizedBox(
+          height: 4,
+        ),
+        Row(
+          children: [
+            Text('\$${expense.amount.toStringAsFixed(2)}'),
+            const Spacer(),
+            Row(
+              children: [
+                Icon(categoryIcons[expense.category]),
+                const SizedBox(
+                  width: 8,
+                ),
+                Text(expense.formattedDate),
+              ],
+            ),
+          ],
+        ),
+      ]),
+    ));
   }
 }
