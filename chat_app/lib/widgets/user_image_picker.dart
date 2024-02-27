ff38947b3eaa9de599193290062712b6e922a4d9
CalvinL93 <calvinl93@yahoo.com>
2024-02-27 10:35:19 -0600

    296

--- chat_app/lib/widgets/user_image_picker.dart
+++ chat_app/lib/widgets/user_image_picker.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
-  const UserImagePicker({super.key, required this.onPickImage});
+  const UserImagePicker({
+    super.key,
+    required this.onPickImage,
+  });
 
   final void Function(File pickedImage) onPickImage;
 
@@ -17,8 +20,12 @@ class UserImagePicker extends StatefulWidget {
 class _UserImagePickerState extends State<UserImagePicker> {
   File? _pickedImageFile;
 
-  void _pickImage() async{
-    final pickedImage = await ImagePicker().pickImage(source: ImageSource.camera, imageQuality: 50, maxWidth: 150,);
+  void _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
 
     if (pickedImage == null) {
       return;
@@ -38,7 +45,8 @@ class _UserImagePickerState extends State<UserImagePicker> {
         CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
-          foregroundImage: _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
+          foregroundImage:
+              _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
         ),
         TextButton.icon(
           onPressed: _pickImage,
